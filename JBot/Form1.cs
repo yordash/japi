53596502de823a02003b3658d24dea41a6c92fa9
XtrmJosh <XtrmJosh@gmail.com>
2013-01-20 00:52:19 +0000

    Added project files.

---
+++ JBot/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace JBot
+{
+    public partial class Form1 : Form
+    {
+        Process p;
+        ReaderClass Read = new ReaderClass();
+        ControlWindow Ctrl = new ControlWindow();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void setWindow(Process Tibia)
+        {
+            Read.Tibia = Tibia;
+            Ctrl.Tibia = Tibia;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            setWindow(Read.getFirstClient());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            HealthLabel.Text = "HP: " + Convert.ToString(Read.Hp()) + "/" + Convert.ToString(Read.MaxHp());
+            ManaLabel.Text = "MP: " + Convert.ToString(Read.Mp()) + "/" + Convert.ToString(Read.MaxMp());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            LocationDisplay.Rows.Add(Convert.ToString(Read.X()), Convert.ToString(Read.Y()), Convert.ToString(Read.Z()));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Objects.BList[] myBlist = Read.BlGet();
+            foreach (Objects.BList creature in myBlist)
+            {
+                if (creature.Id != 0 && creature.Visible == 1)
+                {
+                    blview.Rows.Add(creature.Name, Convert.ToString(creature.Visible), Convert.ToString(creature.Id), Convert.ToString(creature.X), Convert.ToString(creature.Y), Convert.ToString(creature.Z), Convert.ToString(creature.Type));
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            foreach (char c in textBox1.Text)
+            {
+                int charValue = c;
+                string hexValue = charValue.ToString("X");
+                IntPtr val = new IntPtr(c);
+                MessageBox.Show(Convert.ToString(val));
+            }
+            Ctrl.SendKeys(textBox1.Text);
+        }
+
+    }
+}
---
+++ JBot/Objects.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JBot
+{
+    class Objects
+    {
+        public struct BList
+        {
+            public int Addr;
+
+            public int Id;
+            public int Type;
+            public string Name;
+            public int Z;
+            public int Y;
+            public int X;
+            public int TimeLastMoved;
+            public int Walking;
+            public int Direction;
+            public int Previous;
+            public int Next;
+            public int Outfit;
+            public int MountId;
+
+            public int BlackSquare;
+            public int Hppc;
+            public int Speed;
+
+            public int SkullType;
+            public int Party;
+            public int WarIcon;
+            public int Visible;
+        }
+
+        public struct Player
+        {
+            public int Cid;
+            public int Hp;
+            public int Mp;
+            public int HpMax;
+            public int MpMax;
+            public int Exp;
+            public int Soul;
+            public int X;
+            public int Y;
+            public int Z;
+        }
+    }
+}
