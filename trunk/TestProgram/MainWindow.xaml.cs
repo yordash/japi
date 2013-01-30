64ff4cc8c576d45fb9847cc4c44f7ac746075462
XtrmJosh@gmail.com <XtrmJosh@gmail.com@def6a47e-3475-7bfb-0571-92e5d0579183>
2013-01-30 13:40:15 +0000

    Switching project for the API format, now you should compile as a DLL and call functions as demonstrated in the test solution.

---
+++ trunk/TestProgram/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+using JAPI;
+
+namespace TestProgram
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ReaderClass Read = new ReaderClass();
+        Control Ctrl = new Control();
+        Thread Updater;
+        Thread Healer;
+        Objects.HealRule[] rulelist;
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void UpdateStatus()
+        {
+            this.Dispatcher.Invoke(delegate {
+                this.Title = Read.getMyName();
+                XpDisplay.Content = "Xp: " + Read.Exp();
+                HpDisplay.Content = "Hp: " + Read.Hp();
+                MpDisplay.Content = "Mp: " + Read.Mp();
+            });
+
+        }
+
+        private void Startup(object sender, RoutedEventArgs e)
+        {
+            Util.Tibia = Read.getFirstClient();
+            UpdateStatus();
+            Updater = new Thread(UpdateTick);
+            Updater.Start();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Updater.Abort();
+        }
+
+        private void UpdateTick()
+        {
+            while (true)
+            {
+                UpdateStatus();
+                Thread.Sleep(500);
+            }
+        }
+
+        private void HealTick()
+        {
+            /*while (true)
+            {
+                p = Read.GetPlayerInfo();
+                foreach (Objects.HealRule rule in rulelist)
+                {
+                    if (p.Hp < rule.MaxHp && p.Hp > rule.MinHp && rule.Mana <= p.Mp)
+                    {
+                        Ctrl.SendButton(rule.Hotkey);
+                        Thread.Sleep(500);
+                    }
+                }
+            }*/
+        }
+
+        private void CheckBox_Checked_1(object sender, RoutedEventArgs e)
+        {
+            if (checkBox1.IsChecked == true)
+            {
+                int i = 0;
+                rulelist = new Objects.HealRule[listBox1.Items.Count];
+                foreach (string healrule in listBox1.Items)
+                {
+                    string[] rules = healrule.Split(',');
+                    rulelist[i].MinHp = Convert.ToInt32(rules[0]);
+                    rulelist[i].MaxHp = Convert.ToInt32(rules[1]);
+                    rulelist[i].Mana = Convert.ToInt32(rules[2]);
+                    rulelist[i].Hotkey = rules[3];
+                    i++;
+                }
+                Healer = new Thread(HealTick);
+                Healer.IsBackground = true;
+                Healer.Start();
+            }
+            else
+            {
+                Healer.Abort();
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            listBox2.Items.Add("Stand: " + Convert.ToString(Read.X()) + ", " + Convert.ToString(Read.Y()) + ", " + (Convert.ToString(Read.Z())));
+        }
+    }
+}
